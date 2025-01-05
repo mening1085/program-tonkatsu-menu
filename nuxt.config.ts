a49574c25ccee9a3c9d38a0dc17c9c7ebdf93a70
Sungka <kasem.t@kisracorp.com>
2025-01-05 23:32:53 +0700

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  css: ["~/assets/styles/main.scss", "./assets/styles/css/tailwind.css"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "nuxt-aos"],
-  css: ["~/assets/styles/main.scss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "nuxt-aos", "@vite-pwa/nuxt"],
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Program Tonkatsu",
+      short_name: "Program",
+      description: "Program Tonkatsu is a Food Japanese Restaurant Styles",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+  },
 });
